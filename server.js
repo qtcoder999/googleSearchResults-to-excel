d04eb7e1a44b1ac82eb842b8baa214cb78a41fab
qtparas@gmail.com <qtparas@gmail.com>
2018-07-27 12:57:10 +0530

    code

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const args = process.argv;
+
+
 var length = 5;
 var count = 2;
+
 var xl = require('excel4node');
 var Q = require('q');
-var lastRow1 = 0;
-var lastRow2 = 0;
+
+
 var wb = new xl.Workbook();
 var ws = wb.addWorksheet('Sheet 1');
 const {Builder, By, Key, until} = require('selenium-webdriver');
@@ -15,7 +18,9 @@ var s = driver.get('http://www.google.com/ncr')
     .then(_ =>
         driver.findElement(By.name('q')).sendKeys(process.argv[2], Key.RETURN))
     .then(_ => driver.wait(until.titleIs(process.argv[2]+' - Google Search'), 5000))
+
     .then(_ => TakeTextsAndURLs());
+
     function TakeTextsAndURLs(){
         var i=0,j=0,h=0;
         var titles = new Array();
@@ -24,10 +29,12 @@ var s = driver.get('http://www.google.com/ncr')
         var deferred2 = Q.defer();
         var deferred3 = Q.defer();
         var deferred4 = Q.defer();
+
         var promise1 = deferred1.promise;
         var promise2 = deferred2.promise;
         var promise3 = deferred3.promise;
         var promise4 = deferred4.promise;
+
         console.log("taking  titles and URLs");
         //console.log(driver);
         driver.wait(until.titleIs(process.argv[2]+' - Google Search'), 2000).then(function(){
@@ -43,10 +50,13 @@ var s = driver.get('http://www.google.com/ncr')
                         console.log("deferred 1 resolved");
                         deferred1.resolve(text);
                     }
+                    
                 })
             })
         })
         })
+            
+            
         promise1.then(function(){
             driver.wait(until.titleIs(process.argv[2]+' - Google Search'), 2000).then(function(){
             driver.findElements(By.css(".iUh30")).then(function(elements){
@@ -58,6 +68,7 @@ var s = driver.get('http://www.google.com/ncr')
                         if(j==elements.length - 1){
                             console.log("deferred 2 resolved");
                             deferred2.resolve(text);
+                            
                         }
                     });
                 });
@@ -65,6 +76,8 @@ var s = driver.get('http://www.google.com/ncr')
             //driver.quit();
         })
     })
+
+        
     promise1.done(function(text1){
         promise2.done(function(text2){
             console.log(titles);
@@ -75,46 +88,54 @@ var s = driver.get('http://www.google.com/ncr')
                 console.log(i);
                     //console.log(titles[i]);
                     //console.log(URLs[i]);
-                 //ws.cell(lastRow1 + i+1, 2).string(titles[i]);
-                 //ws.cell(lastRow1 + i+1, 1).string(URLs[i]);
+                // ws.cell(i+1, 2).string(titles[i]);
+                // ws.cell(i+1, 1).string(URLs[i]);
                 if(i==titles.length - 1)
                 {
-                    lastRow1 = i;
+                    //wb.write('Excel.xlsx');
                     console.log("Deferred 3 resolved");
                     deferred3.resolve();
                 }
                 i++;
             }
             //console.log("everything executed. J : " + j);
+
         })
     });
+
+    
     promise3.done(function(){
         console.log("reached at promise 3 -> done");
         //wb.write('Excel.xlsx')
         h=2;
+        
         console.log("Recursive function fired for count: "+ count);
+        
         recursiveFn(length, count)
+        
     });
     }
+
+
     function recursiveFn( length, count){
         console.log("inside recursive fn");
         driver.wait(until.titleIs(process.argv[2]+' - Google Search'), 5000).then(function(){
           //  driver.wait(ExpectedConditions.presenceOfAllElementsLocatedBy(By.css("a[aria-label=\"Page "+ count + "\"]")), 5000).then(function(){    
+        
             console.log("recursive fn - inside wait");
             console.log("Value of count:", count);
+
             driver.findElement(By.css("a[aria-label=\"Page "+ count + "\"]")).click().then(function(){
                 //console.log("Value of count:",count)
-                if(count < length){
+                if(count<length){
                     driver.wait(until.titleIs(process.argv[2]+' - Google Search'), 2000).then(function(){
                         count = count + 1;
                         TakeTextsAndURLs()
                         console.log("count incremented!");
+                        
                         console.log("Value of new count:",count);
                     });
                     //recursiveFn(driver, until, length, count)
-                }
-                else{
-                    //wb.write('Excel.xlsx');
                 }
             });
        })        
