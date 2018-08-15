4b9b2611e44ba058d025a71bd59ddec4b810ae6b
qtcoder999 <30284500+qtcoder999@users.noreply.github.com>
2018-08-16 00:42:28 +0530

    Code not working

--- google_search.js
+++ google_search.js
@@ -1,71 +1,70 @@
 const args = process.argv;
-
 var xl = require('excel4node');
 var Q = require('q');
 var lastRow1 = 0;
 var lastRow2 = 0;
 var wb = new xl.Workbook();
 var ws = wb.addWorksheet('Sheet 1');
-const {Builder, By, Key, until} = require('selenium-webdriver');
-
+const { Builder, By, Key, until } = require('selenium-webdriver');
 var driver = new Builder()
     .forBrowser('chrome')
     .build();
-
 var s = driver.get('http://www.google.com/ncr')
     .then(_ =>
         driver.findElement(By.name('q')).sendKeys(process.argv[2], Key.RETURN))
-    .then(_ => driver.wait(until.titleIs(process.argv[2]+' - Google Search'), 5000))
-    .then(_ => recursiveFn(5, 2));
-    
-    function TakeTextsAndURLs(length1, count1){
-        console.log(length1, count1);
-        var i=0,j=0,h=0;
-        var titles = new Array();
-        var URLs = new Array();
-        var deferred1 = Q.defer();
+    .then(_ => driver.wait(until.titleIs(process.argv[2] + ' - Google Search'), 5000))
+    .then(_ => TakeTextsAndURLs());
 
-        var promise1 = deferred1.promise;
 
-        console.log("taking  titles and URLs");
-                
-        driver.wait(until.titleIs(process.argv[2]+' - Google Search'), 2000).then(function(length1,count1){
-            console.log(length1, count1);
-            driver.findElements(By.css(".bkWMgd .g .r>a"),length1,count1).then(function(elements,length1,count1){
-                console.log(length1,count1);
-                console.log("inside findelements");
-                void async function(length1,count1) {
-                        console.log(length1,count1);
-                        await elements.forEach(function (element,index) {
-                            //console.log("inside foreach elements");
-                        void async function() {
-                                    await element.getText().then(function(text){
-                                                //console.log(text);
-                                                titles.push(text);
-                                                i++;
-                                                if(i == elements.length ){
-                                                            console.log("deferred 1 resolved");
-                                                            console.log(titles);
-                                                            deferred1.resolve(text);
-                                                            
-                                                            if(count1 <= length1){
-                                                                    console.log("Inside if");
-                                                                    count1++;
-                                                                    driver.findElement(By.css("a[aria-label=\"Page "+ count1 + "\"]")).click();
-                                                                    recursiveFn(length1, count1);
-                                                            }
-                                                }
-                                    })
-                            }();
+async function TakeTextsAndURLs() {
+    var i = 0, j = 0, h = 0;
+    var titles = new Array();
+    var URLs = new Array();
+    await getTitles(titles);
+    await getURLs(URLs);
+    console.log(titles);
+    console.log(URLs);
+}
+function getURLs(URLs) {
+    var i = 0;
+    driver.wait(until.titleIs(process.argv[2] + ' - Google Search'), 2000).then(function () {
+        driver.findElements(By.css(".iUh30")).then(function (elements) {
+                 elements.forEach(function (element, index) {
+                         element.getText().then(function (text) {
+                            URLs.push(text);
+                            i++;
+                            if (i == elements.length) {
+                                //if (count1 <= length1) {
+                                //count1++;
+                                //driver.findElement(By.css("a[aria-label=\"Page " + count1 + "\"]")).click();
+                                // }
+                            }
                         })
-                }();
-            })
+             
+                })
         })
-    return promise1;
+    })
 }
-async function recursiveFn( length, count){
-        console.log("Inside recursive");
-        console.log(length,count);
-        console.log("Inside async text and URLs");
-        TakeTextsAndURLs(length, count)
+function getTitles(titles) {
+    var i = 0;
+    driver.wait(until.titleIs(process.argv[2] + ' - Google Search'), 2000).then(function () {
+        driver.findElements(By.css(".bkWMgd .g .r>a")).then(function (elements) {
+                 elements.forEach(function (element, index) {
+                         element.getText().then(function (text) {
+                            titles.push(text);
+                           
+                            i++;
+                            if (i == elements.length) {
+                                //if (count1 <= length1) {
+                                //count1++;
+                                //driver.findElement(By.css("a[aria-label=\"Page " + count1 + "\"]")).click();
+                                // }
+                            }
+                        })
+             
+                })
+        
+        })
+    })
 }
+
