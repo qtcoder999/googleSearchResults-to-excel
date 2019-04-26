761298f584d3229c2c8dc28b08b83cbe11ec55cd
Paras Anand <paras.anand@idc.bedbath.com>
2019-04-27 00:52:50 +0530

    One promise working fine

--- google_search.js
+++ google_search.js
@@ -4,21 +4,33 @@ require("chromedriver");
 const { Builder, By, Key, until } = require("selenium-webdriver");
 
 let url = "http://www.google.com/ncr";
-let windowCount = 0;
-let driver = new Builder().forBrowser("chrome").build();
+let windowCount = 0,
+  driver;
 
+async function openChrome() {
+  return new Promise(async function(resolve, reject) {
+    driver = new Builder().forBrowser("chrome").build();
+    driver.then(function() {
+      console.log("openchrome promise resolved");
+      resolve();
+    });
+  });
+}
 async function devMain(item) {
   console.log(item);
 
-  mywindow = await driver.executeScript('window.open("' + url + '"); ');
+  return new Promise(async function(resolve, reject) {
+    // Do async job
+    await driver
+      .get(url)
+      .then(driver.findElement(By.name("q")).sendKeys(item, Key.RETURN))
+      .then(resolve());
+  });
 
-  //await driver.executeScript('mywindow.focus();');
-  await driver.switch_to_window(driver.window_handles[windowCount]);
+  // await driver.executeScript('mywindow = window.open("' + url + '"); ');
 
-  await driver
-    .get(url)
-    .then(driver.findElement(By.name("q")).sendKeys(item, Key.RETURN))
-    .then(++windowCount);
+  // await driver.executeScript("mywindow.focus();");
+  // await driver.switch_to_window(driver.window_handles[windowCount]);
 }
 
 //devMain();
@@ -32,8 +44,9 @@ getData().then(result => {
   passToDev(result[0].data);
 });
 
-const passToDev = arr => {
-  arr.map(item => {
-    devMain(item[0]);
+const passToDev = async arr => {
+  await openChrome();
+  arr.map(async item => {
+    await devMain(item[0]);
   });
 };
