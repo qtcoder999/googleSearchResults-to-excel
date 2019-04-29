fb4a8253dab3d287d7dc9d15a38f162ddc1918f2
Paras Anand <paras.anand@idc.bedbath.com>
2019-04-30 03:46:42 +0530

    Reverting changes due to a bug found in the selenium library

--- google_search.js
+++ google_search.js
@@ -1,40 +1,22 @@
-var excel = require("./excel");
-
+const excel = require("./excel");
 require("chromedriver");
-const { Builder, By, Key, until } = require("selenium-webdriver");
 
-let url = "http://www.google.com/ncr";
-let windowCount = 0,
-  driver;
+const { Builder, By, Key } = require("selenium-webdriver");
+
+let url = "http://www.google.com/ncr",
+  driver,
+  windows,
+  numberOfNewTabsToOpen;
 
 async function openChrome() {
   return new Promise(async function(resolve, reject) {
     driver = new Builder().forBrowser("chrome").build();
     driver.then(function() {
-      console.log("openchrome promise resolved");
+      console.log(Date.now(), "openchrome promise resolved");
       resolve();
     });
   });
 }
-async function devMain(item) {
-  console.log(item);
-
-  return new Promise(async function(resolve, reject) {
-    // Do async job
-    await driver
-      .get(url)
-      .then(driver.findElement(By.name("q")).sendKeys(item, Key.RETURN))
-      .then(resolve());
-  });
-
-  // await driver.executeScript('mywindow = window.open("' + url + '"); ');
-
-  // await driver.executeScript("mywindow.focus();");
-  // await driver.switch_to_window(driver.window_handles[windowCount]);
-}
-
-//devMain();
-let arr;
 
 const getData = async () => {
   return await excel.getDataFromExcel();
@@ -46,7 +28,8 @@ getData().then(result => {
 
 const passToDev = async arr => {
   await openChrome();
-  arr.map(async item => {
-    await devMain(item[0]);
+
+  arr.map(async (item, index) => {
+    const searchTerm = item[0];
   });
 };
