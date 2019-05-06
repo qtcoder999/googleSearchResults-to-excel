69ec8954625b6ea7b2fcdc2216e8dd2617982551
Paras Anand <paras.anand@idc.bedbath.com>
2019-05-06 08:03:51 +0530

    Initial commit

--- google_search.js
+++ google_search.js
@@ -1,65 +1,25 @@
+const { Builder, By, until } = require("selenium-webdriver");
 const excel = require("./excel");
 require("chromedriver");
 
-const { Builder, By, until } = require("selenium-webdriver");
-
-let url = "https://google.com",
-  driver,
-  suffix = "imdb",
-  arrayDuplicate = [];
-
+const url = "https://google.com";
+let driver;
+const suffix = "imdb";
+let arrayDuplicate = [];
 async function openChrome() {
-  return new Promise(async function(resolve, reject) {
+  return new Promise(async resolve => {
     driver = new Builder().forBrowser("chrome").build();
-    driver.then(function() {
+    driver.then(() => {
       resolve();
     });
   });
 }
-
-const getData = async () => {
-  return await excel.getDataFromExcel();
-};
-
-getData().then(result => {
-  passToDev(result[0].data);
-});
-
-const passToDev = async arr => {
-  await openChrome();
-  await openTabs(arr.length - 1);
-  await getHandles();
-  console.log("Search terms history:");
-  await mainLoop(arr, 0);
-};
-
-const mainLoop = async (arr, count) => {
-  if (count <= arr.length - 1) {
-    const searchTerm = arr[count].toString();
-    await openGoogle(searchTerm, count);
-    mainLoop(arr, ++count);
-  }
-};
-const getHandles = async () => {
-  await driver.getAllWindowHandles().then(async function(handles) {
-    arrayDuplicate = handles;
-    /*
-      Selenium issue
-      https://github.com/w3c/webdriver/issues/386
-
-      Using a temporary workaround
-    */
-    arrayDuplicate.push(arrayDuplicate.shift());
-  });
-};
 const openTabs = async count => {
   await driver.executeScript("window.open(); window.focus();");
-  if (count > 1) return openTabs(--count);
+  const newCount = count - 1;
+  if (count > 1) return openTabs(newCount);
+  return null;
 };
-async function openGoogle(searchTerm, index) {
-  await driver.switchTo().window(arrayDuplicate[arrayDuplicate.length - 1]);
-  await searchTheTerm(searchTerm).then(arrayDuplicate.pop());
-}
 const searchTheTerm = async searchTerm => {
   console.log(searchTerm);
   const searchLocator = By.name("q");
@@ -70,10 +30,47 @@ const searchTheTerm = async searchTerm => {
     .get(url)
     .then(
       await driver
-        .findElement(
-          async () => await driver.wait(until.elementLocated(searchLocator))
-        )
+        .findElement(driver.wait(until.elementLocated(searchLocator)))
         .sendKeys(`${searchTerm} ${suffix}`)
     )
     .then(await driver.findElement(iAmFeelingLuckyLocator).click());
 };
+
+async function openGoogle(searchTerm) {
+  await driver.switchTo().window(arrayDuplicate[arrayDuplicate.length - 1]);
+  await searchTheTerm(searchTerm).then(arrayDuplicate.pop());
+}
+
+const mainLoop = async (arr, count) => {
+  if (count <= arr.length - 1) {
+    const searchTerm = arr[count].toString();
+    await openGoogle(searchTerm, count);
+    const newCount = count + 1;
+    mainLoop(arr, newCount);
+  }
+};
+const getHandles = async () => {
+  await driver.getAllWindowHandles().then(async handles => {
+    arrayDuplicate = handles;
+    /*
+      Selenium issue
+      https://github.com/w3c/webdriver/issues/386
+
+      Using a temporary workaround
+    */
+    arrayDuplicate.push(arrayDuplicate.shift());
+  });
+};
+const passToDev = async arr => {
+  await openChrome();
+  await openTabs(arr.length - 1);
+  await getHandles();
+  console.log("Search terms history:");
+  await mainLoop(arr, 0);
+};
+
+const getData = () => excel.getDataFromExcel();
+
+getData().then(result => {
+  passToDev(result[0].data);
+});
